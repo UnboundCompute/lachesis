46257e47b63bb963b767bef0632de15466ddf250
Riyan Dhiman <riyandhiman14@gmail.com>
2026-08-25 19:56:35 +0530

    chore: exclude native build artifacts

--- native/lifetime_kernel/target/debug/deps/lachesis_lifetime_kernel-f87731be7190cfa3.d
+++
@@ -1,5 +0,0 @@
-/Users/riyandhiman/project/unboundcompute/arachne/native/lifetime_kernel/target/debug/deps/lachesis_lifetime_kernel-f87731be7190cfa3.d: src/lib.rs
-
-/Users/riyandhiman/project/unboundcompute/arachne/native/lifetime_kernel/target/debug/deps/lachesis_lifetime_kernel-f87731be7190cfa3: src/lib.rs
-
-src/lib.rs:
--- native/lifetime_kernel/target/debug/deps/lachesis_lifetime_kernel.d
+++
@@ -1,7 +0,0 @@
-/Users/riyandhiman/project/unboundcompute/arachne/native/lifetime_kernel/target/debug/deps/lachesis_lifetime_kernel.d: src/lib.rs
-
-/Users/riyandhiman/project/unboundcompute/arachne/native/lifetime_kernel/target/debug/deps/liblachesis_lifetime_kernel.rlib: src/lib.rs
-
-/Users/riyandhiman/project/unboundcompute/arachne/native/lifetime_kernel/target/debug/deps/liblachesis_lifetime_kernel.dylib: src/lib.rs
-
-src/lib.rs:
